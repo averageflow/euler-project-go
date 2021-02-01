80c0d118b27eb693bffa5130e22ef099b716b59b
J. J <josep@prowarehouse.nl>
2021-02-01 22:15:12 +0100

    add main go file

--- problems/1-100/[3] Largest Prime Factor/main.go
+++ problems/1-100/[3] Largest Prime Factor/main.go
@@ -1,1 +1,38 @@
-package _3__Largest_Prime_Factor
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func IsPrime(n int64) bool {
+	for i := int64(2); i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	start := time.Now()
+
+	wantedNumber := int64(600851475143)
+
+	max := int64(0)
+	for i := int64(2); i <= wantedNumber; i++ {
+		if wantedNumber%i == 0 {
+			if IsPrime(i) {
+				max = i
+			}
+			wantedNumber /= i
+			i = 2
+		}
+	}
+
+	fmt.Printf(
+		"\nExecution time: %v\nResult: %d\n\n",
+		time.Since(start),
+		max,
+	)
+}
