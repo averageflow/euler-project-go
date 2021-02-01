e767478720664b5694e01a540d8cf1770509c5ea
J. J <josep@prowarehouse.nl>
2021-02-01 21:53:29 +0100

    Add main.go files

--- problems/1-100/[2]Fibonacci/main.go
+++ problems/1-100/[2]Fibonacci/main.go
@@ -1,1 +1,42 @@
-package _2_Fibonacci
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	var resultingItems []int64
+
+	resultingItems = append(resultingItems, 0)
+	resultingItems = append(resultingItems, 1)
+
+	i := 2
+
+	for {
+		sum := resultingItems[i-1] + resultingItems[i-2]
+
+		if sum < 4000000 {
+			resultingItems = append(resultingItems, sum)
+			i++
+		} else {
+			break
+		}
+	}
+
+	var result int64
+
+	for j := range resultingItems {
+		if resultingItems[j]%2 == 0 {
+			result += resultingItems[j]
+		}
+	}
+
+	fmt.Printf(
+		"\nExecution time: %v\nResult: %d\n\n",
+		time.Since(start),
+		result,
+	)
+}
